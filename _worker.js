ce2c18e8228c4d245debb0e444ad995a5b3d5ecb
Alien-Et <my-melody1@live.com>
2025-09-28 04:10:23 +0800

    更新 _worker.js

--- _worker.js
+++ _worker.js
@@ -2490,8 +2490,9 @@ rules:
 `;
 
 if (b64Enabled) {
-    // 添加 base64 标记，确保 Clash 能正确识别和解密
-    return '#!base64 ' + btoa(unescape(encodeURIComponent(配置文本)));
+  // 先添加 base64 标记，再对配置内容进行加密
+  // 注意：标记本身不加密，只加密配置内容
+  return '#!CLASH=1 ' + btoa(unescape(encodeURIComponent(配置文本)));
 }
 return 配置文本;
 }
